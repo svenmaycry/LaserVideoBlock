1a43ffd91931f35acb3ffc63d9ee91688a1760f9
SvenMayCry <svenmaycry@gmail.com>
2024-04-16 15:13:30 +0300

    ФУнкционал с модалкой и воспроизведением видео при наведении.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// const typeCutItems = document.querySelectorAll('.js-type-cut-item');
+// typeCutItems.forEach(item => {
+//     const typeCutVideo = item.querySelector('video');
+//
+//     item.addEventListener('mouseenter', function () {
+//         typeCutVideo.play();
+//     });
+//
+//     item.addEventListener('mouseleave', function () {
+//         typeCutVideo.pause();
+//         typeCutVideo.currentTime = 0;
+//     });
+// });
